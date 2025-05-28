87a345756f1d506abd777b5dd6f0fdd85eb7d8a7
Andrew Schweitzer <schweitz72@gmail.com>
2025-05-28 13:41:03 -0400

    blacken

--- src/gwdcli/events/queue_loop.py
+++ src/gwdcli/events/queue_loop.py
@@ -1,5 +1,5 @@
 """Generic message processor inside asyncio event loop. Probably unnecessary. Should probably be removed and messages
-should instead be directly fed to TUI's sync queue. """
+should instead be directly fed to TUI's sync queue."""
 
 import asyncio
 import queue
